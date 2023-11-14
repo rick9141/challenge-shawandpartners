c7c2ee54186f0ecd30f9c9486ea19459693cc364
Luis Henrique <gavirate.henrique@gmail.com>
2023-11-13 21:55:40 -0300

    Update merge update projects

--- ShawAndPartners.API/Program.cs
+++ ShawAndPartners.API/Program.cs
@@ -1,11 +1,31 @@
+using Microsoft.OpenApi.Models;
+using ShawAndPartners.API;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Inicializa o banco de dados
+SQLitePCL.Batteries_V2.Init();
+ServiceExtensions.InitializeDatabase(builder.Configuration);
+
 // Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddInjections(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Challenge - Shaw And Partners", Version = "v1" });
+    c.IncludeXmlComments(xmlPath);
+});
+
+
+
 
 var app = builder.Build();
 
