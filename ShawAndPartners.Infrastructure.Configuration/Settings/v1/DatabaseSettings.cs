acb629c485b3ad90a60cce952b4751e1459920fe
Luis Henrique <gavirate.henrique@gmail.com>
2023-11-13 22:01:22 -0300

    Add configuration db

---
+++ ShawAndPartners.Infrastructure.Configuration/Settings/v1/DatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace ShawAndPartners.Infrastructure.Configuration.Settings.v1
+{
+    public class DatabaseSettings
+    {
+        public long ConnectionTimeout { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
